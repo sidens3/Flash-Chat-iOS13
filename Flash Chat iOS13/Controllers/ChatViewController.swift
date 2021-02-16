e93b2389bfe7c5a07d27b0f2350756c88a8528eb
Mikhail.Zinovev <31514294+sidens3@users.noreply.github.com>
2021-02-16 10:32:44 +0300

    add nib for cell

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,9 +14,20 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages : [Message] = [
+        Message(sender: "1@2.com", body: "hey"),
+        Message(sender: "1@b.com", body: "Hello"),
+        Message(sender: "1@c.com", body: "Well")
+        
+        ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = K.
+        
+    
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+        title = K.appName
         
         navigationItem.hidesBackButton = true
         
@@ -35,4 +46,21 @@ class ChatViewController: UIViewController {
         }
     }
     
+}
+
+//MARK: - UITableViewDataSource
+
+extension ChatViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        K.cellIdentifier
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        
+        cell.lable.text = messages[indexPath.row].body
+        return cell
+        
+    }
 }
